7f0144562bfebce53e6c6b2997c119b503ca3054
lavanya gunnu <lavanyagunnu01@gmail.com>
2024-10-27 15:56:04 +0000

    first commit

---
+++ start.sh
@@ -0,0 +1,9 @@
+# sudo apt update
+# sudo apt install python3.10-venv
+# rm -rf /workspace/ProjectHMS/.venv
+# activate python3.10 vene .venv using - source /workspace/ProjectHMS/.venv/bin/activate
+# flask run
+# docker build -t rest-apis-flask-python .
+# docker run -p 5005:5000 rest-apis-flask-python
+# docker compose up
+# docker compose up --build --force-recreate --no-deps web
