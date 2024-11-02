441b8bfe361c82432c52b2c435a4d2a01426de23
lavanya gunnu <lavanyagunnu01@gmail.com>
2024-11-02 03:56:16 +0000

    smorest

--- start.sh
+++ start.sh
@@ -7,3 +7,6 @@
 # docker run -p 5005:5000 rest-apis-flask-python
 # docker compose up
 # docker compose up --build --force-recreate --no-deps web
+# docker build -t flask-smorest-api .
+# docker run -dp 5000:5000 flask-smorest-api
+# docker run -dp 5005:5000 -w /app -v "$(pwd):/app" flask-smorest-api
